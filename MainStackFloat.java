2c7cc1232f2d86a0c8db4cce54256731302c35c2
asianur <43776916+asianur@users.noreply.github.com>
2018-11-01 11:03:20 +0800

    Create MainStackFloat.java

---
+++ MainStackFloat.java
@@ -0,0 +1,19 @@
+package tugasselanjutnya;
+
+public class MainStackFloat{
+
+	public static void main(String[] args) {
+		StackFloat BeratBadan = new StackFloat();
+		BeratBadan.cetak();
+		
+		BeratBadan.push((float)45.5);BeratBadan.cetak();
+		BeratBadan.push((float)34.5);BeratBadan.cetak();
+		BeratBadan.push((float)45.4);BeratBadan.cetak();
+		BeratBadan.push((float)54.5);BeratBadan.cetak();
+
+		Double BB = BeratBadan.pop();BeratBadan.cetak();
+		System.out.println(BeratBadan);
+
+	}
+
+}
