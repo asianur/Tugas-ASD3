1f2b9fc66839e6572fa547e3ce5235cfd23707a5
asianur <43776916+asianur@users.noreply.github.com>
2018-11-01 11:04:22 +0800

    Create MainStackString.java

---
+++ MainStackString.java
@@ -0,0 +1,22 @@
+package tugasselanjutnya;
+
+public class MainStackString {
+
+	public static void main(String[] args) {
+		StackString Buah = new StackString();
+		Buah.cetak();
+		
+		Buah.push("rambutan");Buah.cetak();
+		Buah.push("durian");Buah.cetak();
+		Buah.push("jeruk");Buah.cetak();
+		
+
+		String ListBuah = Buah.pop();Buah.cetak();
+		System.out.println(ListBuah);
+		
+	}
+
+
+
+	}
+
